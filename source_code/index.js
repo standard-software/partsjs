ae363cd8704bd7fe904cfd583cdf5c2c5264c927
standard-software <standard.software.net@gmail.com>
2019-08-29 19:10:15 -0400

    babel v6 install

--- source_code/index.js
+++ source_code/index.js
@@ -1,6 +1,11 @@
 const parts = {};
 
-parts.VERSION = '0.1.1';
+parts.VERSION = '0.2.0 beta';
+
+parts.test_babel = () => {
+  const array = [1, 2, 3];
+  console.log(array.map(n => n ** 2));
+};
 
 module.exports = parts;
 
--- test_code/test_index.js
+++ test_code/test_index.js
@@ -1,3 +1,4 @@
 const parts = require('../source_code/index.js');
 
 console.log(parts.VERSION);
+parts.test_babel();
