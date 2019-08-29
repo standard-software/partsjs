e01fdf0b0a758197b066e3267f4e994f991043dc
standard-software <standard.software.net@gmail.com>
2019-08-29 19:57:26 -0400

    babel config change

---
+++ babel.config.js
@@ -0,0 +1,4 @@
+const presets =  [
+  ["@babel/preset-env"]
+];
+module.exports = { presets }
---
+++ debug_build/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var parts = {};
+parts.VERSION = '0.2.0 beta';
+
+parts.test_babel = function () {
+  var array = [1, 2, 3];
+  console.log(array.map(function (n) {
+    return Math.pow(n, 2);
+  }));
+};
+
+module.exports = parts;
