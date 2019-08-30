b0440fe38510966e3c73567eddbbec6ca42f846d
standard-software <standard.software.net@gmail.com>
2019-08-30 16:40:29 -0400

    change webpack.config position

--- webpack_config/webpack.config.js
+++ webpack_config/webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: ['./debug_build/index.js'],
   output: {
     filename: 'parts.js',
-    path: __dirname + '/release_build/',
+    path: __dirname + '/../release_build/',
     library: '',
     libraryTarget: 'commonjs2',
   }
