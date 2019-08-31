523e92cf373f4082ec8f7f38a12d7721cc70b3b3
standard-software <standard.software.net@gmail.com>
2019-08-31 10:37:06 -0400

    change test code file structure

--- test_code/debug/test_index.js
+++ test_code/debug/test_index.js
@@ -1,27 +1,9 @@
 const parts = require('../../debug_build/index.js');
 
 const {
-  test_isUndefined,
-  test_isNull,
-  test_isBoolean,
-  test_isNumber,
-  test_isInteger,
-  test_isString,
-  test_isObject,
-  test_isArray,
-  test_isDate,
-  test_isExcection,
-} = require('./test_type.js');
+  test_execute_type,
+} = require('../test_type.js');
 
 console.log('test start', parts.VERSION);
-test_isUndefined();
-test_isNull();
-test_isBoolean();
-test_isNumber();
-test_isInteger();
-test_isString();
-test_isObject();
-test_isArray();
-test_isDate();
-test_isExcection();
+test_execute_type(parts);
 console.log('test finish.');
