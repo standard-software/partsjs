ae800dceeb106fc02776dda2ae3e508d40f6ed0f
standard-software <standard.software.net@gmail.com>
2019-09-02 11:27:57 -0400

    change file structure

--- test_code/release/test_index.js
+++ test_code/release/test_index.js
@@ -2,7 +2,7 @@ const parts = require('../../release_build/parts.js');
 
 const {
   test_execute_type,
-} = require('../test_type.js');
+} = require('../../source_code/type.test.js');
 
 console.log('test start', parts.VERSION);
 test_execute_type(parts);
--- test_code/source/test_index.js
+++ test_code/source/test_index.js
@@ -2,7 +2,7 @@ const parts = require('../../source_code/index.js');
 
 const {
   test_execute_type,
-} = require('../test_type.js');
+} = require('../../source_code/type.test.js');
 
 console.log('test start', parts.VERSION);
 test_execute_type(parts);
