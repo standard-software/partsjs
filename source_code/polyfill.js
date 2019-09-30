95b5c27f1e6244353dd9e249bf591fe216dce087
standard-software <standard.software.net@gmail.com>
2019-10-01 08:01:35 +0900

    polyfill Object.entries

--- source_code/polyfill.js
+++ source_code/polyfill.js
@@ -158,6 +158,19 @@ const polyfillDefine = () => {
         return false;
     }
   }
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+  if (!Object.entries) {
+    Object.entries = function( obj ){
+      var ownProps = Object.keys( obj ),
+          i = ownProps.length,
+          resArray = new Array(i); // preallocate the Array
+      while (i--)
+        resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+      return resArray;
+    };
+  }
 };
 
 polyfillDefine();
