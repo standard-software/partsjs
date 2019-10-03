7eb17f6a036584a5ca28f1c02a43cca4f78a4d74
standard-software <standard.software.net@gmail.com>
2019-10-03 13:44:00 +0900

    mv webpack.config.js

--- source_code/index.js
+++ source_code/index.js
@@ -8,7 +8,7 @@ const _string = require('./string.js');
 const _consoleHook = require('./consoleHook.js');
 const _object = require('./object.js');
 
-const VERSION = '2.1.1';
+const VERSION = '2.2.0 beta';
 
 // Public Property
 const type = _object._copyProperty(_type,
