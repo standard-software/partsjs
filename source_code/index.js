aeeb7ce9a1bf65b2ea952024b2159c3ceea216a9
standard-software <standard.software.net@gmail.com>
2020-06-15 23:41:49 +0900

    v5.8.0

--- source_code/index.js
+++ source_code/index.js
@@ -12,7 +12,7 @@ const _object = require('./object/object.js');
 const _array = require('./array/array.js');
 const _consoleHook = require('./consoleHook/consoleHook.js');
 
-const VERSION = '5.8.0 beta';
+const VERSION = '5.8.0';
 
 const rootNames = {};
 const propertyNames = {};
