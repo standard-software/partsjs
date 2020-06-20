8a997fb3b90a183702d96f959e33515c0aebe22b
standard-software <standard.software.net@gmail.com>
2020-06-20 18:21:10 +0900

    tool:argstest resolve path

--- tool/argstest.js
+++ tool/argstest.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 console.log('argstest.js');
 
 console.log(process.argv.length);
 
-for (let i = 0; i < process.argv.length; i += 1) {
+for (let i = 2; i < process.argv.length; i += 1) {
   console.log(process.argv[i]);
+  console.log(path.resolve(process.argv[i]));
 }
 
