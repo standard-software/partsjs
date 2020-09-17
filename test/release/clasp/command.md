3bbd84447669c511ec8514032783f2a400970ef2
standard-software <standard.software.net@gmail.com>
2020-09-17 10:46:09 +0900

    update test_parts_standalone

---
+++ test/release/clasp/command.md
@@ -0,0 +1,12 @@
+# install
+> npm i -g @google/clasp
+
+# create
+> clasp create
+> sheets
+
+# pull
+> clasp pull
+
+# push
+> clasp push
---
+++ test/release/clasp/test_parts_standalone_Rhino/Code.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  Logger.log('start test_parts');
+  test_parts.test_execute_index(parts);
+  Logger.log('end test_parts');
+}
