e622e73938bf36e84e0b31a7fc6ec3038a916643
standard-software <standard.software.net@gmail.com>
2020-09-17 10:51:35 +0900

    update test_parts_spreadsheet

--- test/release/clasp/test_parts_spreadsheet_v8/Code.js
+++ test/release/clasp/test_parts_spreadsheet_v8/Code.js
@@ -1,5 +1,18 @@
+function onOpen() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+  var entries = [
+    {
+      name : 'execute myFunction',
+      functionName : 'myFunction',
+    },
+  ];
+  sheet.addMenu('run script', entries);
+};
+
 function myFunction() {
   Logger.log('start test_parts');
   test_parts.test_execute_index(parts);
   Logger.log('end test_parts');
+
+  Browser.msgBox('test finish.');
 }
